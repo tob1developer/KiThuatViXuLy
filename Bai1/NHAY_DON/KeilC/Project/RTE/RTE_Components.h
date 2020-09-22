5e52044adabc1a23bde2fda11d61535958ccf496
unknown <ngotuankiet@github.com>
2020-09-22 09:49:38 -0700

    test proteus

--- Bai1/NHAY_DON/KeilC/Project/RTE/RTE_Components.h
+++ Bai1/NHAY_DON/KeilC/Project/RTE/RTE_Components.h
@@ -3,7 +3,7 @@
  * Auto generated Run-Time-Environment Component Configuration File
  *      *** Do not modify ! ***
  *
- * Project: 'LED_8' 
+ * Project: 'LED_DON' 
  * Target:  'Target 1' 
  */
 
