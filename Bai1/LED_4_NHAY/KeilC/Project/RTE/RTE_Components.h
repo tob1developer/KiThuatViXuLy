d65606d592817485b776d9b48f4474c15c9d7d4f
unknown <ngotuankiet@github.com>
2020-09-22 19:54:22 -0700

    them project LED_4_NHAY + proteus

---
+++ Bai1/LED_4_NHAY/KeilC/Project/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'LED_4_NHAY' 
+ * Target:  'LED_4_NHAY' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
