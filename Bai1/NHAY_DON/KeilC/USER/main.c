91336aa4c8434e9723ca3e4dec0723bf83a5fcae
ngotuankiet <ngotuankiet@github.com>
2020-09-23 09:25:13 +0700

    setting fun

--- Bai1/NHAY_DON/KeilC/USER/main.c
+++ Bai1/NHAY_DON/KeilC/USER/main.c
@@ -7,27 +7,57 @@ void GPIO_Config(void);
 void Clock_Config(void);
 void Delay(uint32_t);
 
+
+//setting 1 den led A
+void Single_Led_Port_A_Setting(GPIO_InitTypeDef GPIO_InitStructure);
+void Single_Led_Port_A();
+
 int main(void)
 {
     Clock_Config();
     SystemCoreClockUpdate();
     GPIO_Config();
+    // while (1)
+    // {
+    //     // GPIO_SetBits(GPIOC, GPIO_Pin_13);
+    //     // Delay(100);
+    //     // GPIO_ResetBits(GPIOC, GPIO_Pin_13);
+    //     // Delay(100);
+
+    //     GPIO_SetBits(GPIOA, GPIO_Pin_13);
+    //     Delay(100);
+    //     GPIO_ResetBits(GPIOA, GPIO_Pin_13);
+    //     Delay(100);
+    // }
+    Single_Led_Port_A();
+		// VDDA vs VSSA la gi ?
+
+
+    return 0;
+}
+
+void Single_Led_Port_A_Setting(GPIO_InitTypeDef GPIO_InitStructure){
+
+    /*enble clock for GPIOA*/
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+
+	/*Configuration GPIO pin*/
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_InitStructure.GPIO_Pin  = GPIO_Pin_13;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
+	
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+}
+
+// nhay 1 den o port A - position 13
+void Single_Led_Port_A(){
     while (1)
     {
-        // GPIO_SetBits(GPIOC, GPIO_Pin_13);
-        // Delay(100);
-        // GPIO_ResetBits(GPIOC, GPIO_Pin_13);
-        // Delay(100);
-
         GPIO_SetBits(GPIOA, GPIO_Pin_13);
         Delay(100);
         GPIO_ResetBits(GPIOA, GPIO_Pin_13);
         Delay(100);
     }
-		// VDDA vs VSSA la gi ?
-
-
-    return 0;
 }
 /*Delay tuong doi*/
 
@@ -50,18 +80,20 @@ void GPIO_Config()
 {
     GPIO_InitTypeDef GPIO_InitStructure;
     
-    // /*enble clock for GPIOC*/
-	// RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+    // // /*enble clock for GPIOC*/
+	// // RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
     
-    /*enble clock for GPIOA*/
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+    // /*enble clock for GPIOA*/
+    // RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
 
-	/*Configuration GPIO pin*/
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-	GPIO_InitStructure.GPIO_Pin  = GPIO_Pin_13;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
-	// GPIO_Init(GPIOC, &GPIO_InitStructure);
-    GPIO_Init(GPIOA, &GPIO_InitStructure);
+	// /*Configuration GPIO pin*/
+	// GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	// GPIO_InitStructure.GPIO_Pin  = GPIO_Pin_13;
+	// GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
+	// // GPIO_Init(GPIOC, &GPIO_InitStructure);
+    // GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+    Single_Led_Port_A_Setting(GPIO_InitStructure);
 }
 
 void Clock_Config(void)
